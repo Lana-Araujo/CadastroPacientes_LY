d44d5eb0a8193216cebf4e725e5d5bb4d2ffc912
Lana Araújo <lana.academico2@gmail.com>
2023-08-25 14:38:37 -0300

    js 1

---
+++ CadastroPacientes - LANA E YAS/main.js
@@ -0,0 +1,21 @@
+class Pacientes {
+    constructor(nome, cpf, endereco, telefone, plano, historico) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.endereco = endereco;
+        this.telefone = telefone;
+        this.plano = plano;
+        this.historico = [];
+    }
+
+    cadastrarPaciente() {
+        document.write(`Ó seu nome é: ${this.nome}.
+        Seu CPF é: ${this.cpf}.
+        Seu endereço é: ${this.endereco}
+        Seu telefone é: ${this.telefone}.
+        Seu plano é: ${this.plano}.`)
+    }
+
+
+
+}
